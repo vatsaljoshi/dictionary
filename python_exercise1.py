05d5f4af6857c20e59cf2495652182f5b8df7b6a
vatsaljoshi <85382481+vatsaljoshi@users.noreply.github.com>
2021-06-07 22:00:29 +0530

    Add files via upload

---
+++ python_exercise1.py
@@ -0,0 +1,28 @@
+#CREATE AN DICTIONARY AND TAKE INPUT FROM THE USER AND RETURN THE MEANING OF THE WORD FROM THE DICTIONARY
+
+"""
+d1={"mutable":"whose meaning can change","unmutable":"meaning cannot change","variable":"whose value keeps on changing"}
+print(d1)
+
+print("now whose meaning do you want to know from the above words???")
+takeinput=input()
+
+print(d1[takeinput])
+#mistakes encountered
+"""
+#1.big brackets to be used for variable calling
+#2.didn't knew about input()
+"""
+"""
+
+
+
+
+#PRACTICE AGAIN AND AGAIN
+#creating a dictionary and taking input from user and then returning the menaing of the word from the dictionary
+d1={"me":"myself","family":"the thing that i love the most","home":"heaven","enemy":"i never want to make them"}
+print(d1)
+print("now in the above words, which word do you want to find meaning of?")
+wordinput=input()
+
+print(d1[wordinput])
